85835c37f4fe6623c586aafe61404b696d7e8e81
dam113 <dam113@PC2013.aulas.local>
2017-05-31 13:41:04 +0200

    Subida

---
+++ src/pruebalibro/Pruebalibro.java
@@ -0,0 +1,28 @@
+
+package pruebalibro;
+
+import biblioteca.Libro;
+import calculos.CGrados;
+import calculos.Nif;
+import juegos.Barco;
+import juegos.Barco1;
+/**
+ * 
+ * @author dam113
+ */
+public class Pruebalibro {
+/**
+ * 
+ * @param args 
+ */
+    public static void main(String[] args) {
+        
+        CGrados cgrados = new CGrados();
+        Libro libro = new Libro("hola","Juan");
+        Barco barco = new Barco();
+        Barco1 barco1 = new Barco1();
+        Nif nif = new Nif(); 
+        
+    }
+    
+}
