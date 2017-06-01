3b49550ede0fc27041c1be71642afb11af17d776
RafaelLc <rafaellodeirocosta4@gmail.com>
2017-06-01 19:50:57 +0200

    Update Pruebalibro.java

--- src/pruebalibro/Pruebalibro.java
+++ src/pruebalibro/Pruebalibro.java
@@ -16,7 +16,7 @@ public class Pruebalibro {
  * @param args 
  */
     public static void main(String[] args) {
-        
+        System.out.println("Hola");
         CGrados cgrados = new CGrados();
         Libro libro = new Libro("hola","Juan");
         Barco barco = new Barco();
